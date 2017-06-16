5bcf8e94a222d3618347acda9ea0438a06be841f
animadelgence <anima.delgence@gmail.com>
2017-06-16 17:03:29 +0530

    Create new page

---
+++ module/Profile/src/Profile/Controller/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Profile\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Zend\Session\Container;
+use Zend\Session\Config\StandardConfig;
+use Zend\Session\SessionManager;
+
+class PageController extends AbstractActionController {
+
+    public function indexAction() {
+        echo "work in progress";exit;
+
+       }
+}
---
+++ module/Profile/src/Profile/Model/pagedetailsTable.php
@@ -0,0 +1,15 @@
+<?php
+	namespace Profile\Model;
+    use Zend\Db\TableGateway\TableGateway;
+    use Zend\Db\ResultSet\ResultSet;
+    use Zend\Db\Sql\Sql;
+    class pagedetailsTable
+    {
+        protected $tableGateWay;
+        public function __construct(TableGateway $tableGateway)
+        {
+            $this->tableGWay = $tableGateway;
+        }
+
+    }
+?>

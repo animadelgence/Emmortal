534fabfbbf8beae3a6f6746cc9768da167c2b480
animadelgence <anima.delgence@gmail.com>
2017-06-20 16:36:34 +0530

    chk

--- public/databasechk.php
+++ public/databasechk.php
@@ -2,7 +2,7 @@
 ini_set('display_error',1);
 error_reporting(E_ALL);
 echo $_SERVER['DOCUMENT_ROOT']."/setting.json";
-$string = file_get_contents($_SERVER['DOCUMENT_ROOT']."/setting.json");
+echo $string = file_get_contents($_SERVER['DOCUMENT_ROOT']."/setting.json");
 $json = json_decode($string, true);
 echo $json['database']["database_host"].$json['database']["database_username"].$json['database']["database_password"].$json['database']["database_name"];
 $con = mysqli_connect($json['database']["database_host"],$json['database']["database_username"],$json['database']["database_password"],$json['database']["database_name"]);

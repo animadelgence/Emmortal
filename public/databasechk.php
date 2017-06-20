42db3b4d6d26597f3de2eea1583a7362261dbd49
animadelgence <anima.delgence@gmail.com>
2017-06-20 16:25:01 +0530

    chk

--- public/databasechk.php
+++ public/databasechk.php
@@ -1,6 +1,7 @@
 <?php
 $string = file_get_contents($_SERVER['DOCUMENT_ROOT']."/setting.json");
 $json = json_decode($string, true);
+echo $json['database']["database_host"].$json['database']["database_username"].$json['database']["database_password"].$json['database']["database_name"];
 $con = mysqli_connect($json['database']["database_host"],$json['database']["database_username"],$json['database']["database_password"],$json['database']["database_name"]);
 $sql = "INSERT INTO user (emailid) VALUES ('john@example.com')";
 

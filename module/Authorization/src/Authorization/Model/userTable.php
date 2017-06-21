1570e47a4d3d07be9698975eb926dde2069462aa
maitrayee <maitrayeedelgence@gmail.com>
2017-06-21 11:44:24 +0530

    chk

--- module/Authorization/src/Authorization/Model/userTable.php
+++ module/Authorization/src/Authorization/Model/userTable.php
@@ -12,13 +12,12 @@
         }
         public function fetchall($query)
         {
-<<<<<<< HEAD
+
             //print_r($query); exit;
             if (empty($query)){
-            $resultSet = $this->tableGWay->select();
+                $resultSet = $this->tableGWay->select();
             }else{
-=======
->>>>>>> origin/master
+
             $resultSet = $this->tableGWay->select($query);
             foreach ($resultSet as $rSet) {
                 $array[] = array(

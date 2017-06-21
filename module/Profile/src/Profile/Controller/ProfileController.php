cbde650d78d82b6d782f4f999d129e3c798ab855
rituparna2311 <rituparna2311@gmail.com>
2017-06-21 12:27:53 +0530

    merging after creating video folder

--- module/Profile/src/Profile/Controller/ProfileController.php
+++ module/Profile/src/Profile/Controller/ProfileController.php
@@ -12,9 +12,14 @@ class ProfileController extends AbstractActionController {
 
     
     public function __construct() {
-
         $userSession = new Container('userloginId');
         $this->sessionid = $userSession->offsetGet('userloginId');
+        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+        $dynamicPath = $protocol . $_SERVER['HTTP_HOST'];
+        if ($this->sessionid == "") {
+            header("Location:" . $dynamicPath. "/album/showalbum");
+            exit;
+        }
     }
     public function indexAction() {
         echo "work in progress";exit;

2d86abf67765231a3964cca19db74f68b8015d7e
Rajyasree <rajyasree.delgence@gmail.com>
2017-06-21 14:57:39 +0530

    searching stuffs

--- public/js/profile/searchmodal.js
+++ public/js/profile/searchmodal.js
@@ -1,4 +1,3 @@
-
 /*
  * @Author: Rajyasree
  * @Date:   2017-06-21 17:46:35
@@ -10,33 +9,82 @@
 /*jslint plusplus: true */
 /*jshint -W065 */
 /*global baseUrl*/
+/*jslint eqeq: true*/
+var base_url_dynamic = window.location.origin;
 $(document).ready(function () {
     "use strict";
-    $("#allTab").trigger("click");
-    //alert('search moda js calling');
+    $('#allTabShow').css('display', 'block');
     $('body').on('click', '#allTab', function () {
-        alert();
+        $("#li-allTab").addClass("active");
+        $("#li-relationshipTab").removeClass("active");
+        $("#li-incomingTab").removeClass("active");
+        $("#li-outgoingTab").removeClass("active");
         $('#allTabShow').css('display', 'block');
         $('#relationshipTabShow').css('display', 'none');
         $('#incomingTabShow').css('display', 'none');
         $('#outgoingTabshow').css('display', 'none');
     });
     $('body').on('click', '#relationshipTab', function () {
+        $("#li-relationshipTab").addClass("active");
+        $("#li-allTab").removeClass("active");
+        $("#li-incomingTab").removeClass("active");
+        $("#li-outgoingTab").removeClass("active");
         $('#relationshipTabShow').css('display', 'block');
         $('#allTabShow').css('display', 'none');
         $('#incomingTabShow').css('display', 'none');
         $('#outgoingTabshow').css('display', 'none');
     });
     $('body').on('click', '#incomingTab', function () {
+        $("#li-incomingTab").addClass("active");
+        $("#li-allTab").removeClass("active");
+        $("#li-relationshipTab").removeClass("active");
+        $("#li-outgoingTab").removeClass("active");
         $('#incomingTabShow').css('display', 'block');
         $('#relationshipTabShow').css('display', 'none');
         $('#allTabShow').css('display', 'none');
         $('#outgoingTabshow').css('display', 'none');
     });
     $('body').on('click', '#outgoingTab', function () {
+        $("#li-outgoingTab").addClass("active");
+        $("#li-allTab").removeClass("active");
+        $("#li-incomingTab").removeClass("active");
+        $("#li-relationshipTab").removeClass("active");
         $('#outgoingTabshow').css('display', 'block');
         $('#relationshipTabShow').css('display', 'none');
         $('#allTabShow').css('display', 'none');
         $('#incomingTabShow').css('display', 'none');
+    });
+    $('body').on('keyup', '#searchText', function () {
+        var friendsid = $(this).val().trim();
+        alert(friendsid);
+        if (friendsid != '') { // jshint ignore:line
+            $.ajax({
+                type: "POST",
+                url: base_url_dynamic + '/friendrequests/searchfriends',
+                data: {},
+                success: function (res) {
+                   // console.log(res);
+                    //return false;
+                    var jsObject = JSON.parse(res);
+                    var html = "",
+                        profileimage = "/image/bg-30f1579a38f9a4f9ee2786790691f8df.jpg";
+                    for (var i = 0; i < jsObject.userDetails.length; i++) {
+                        var id = jsObject.userDetails[i].friendsid;
+                        var friendsname = jsObject.userDetails[i].friendsname.toLowerCase();
+                        if (friendsname.indexOf(friendsid.toLowerCase()) > -1) {
+                            if ($.inArray(parseInt(id), frndDetails) == '-1') {
+                                if (jsObject.userDetails[i].profileimage != null) {
+                                    profileimage = jsObject.userDetails[i].profileimage;
+                                }
+                                html += '<li class="frndlist-click-class dropdown-li" id="frndlist-click-image" data-id="' + jsObject.userDetails[i].friendsid + '"><img src="' + profileimage + '" class="img-circle frnd-image-class" alt="Cinque Terre" ><span class="frnd-list-name" id="frnd-list-name-id">' + jsObject.userDetails[i].friendsname + '</span></li>';
+                            }
+                        }
+                    }
+                    alert(html);
+                    /*$('#frndlistImage').html(html);
+                    $('#frndlistImage').show();*/
+                }
+            });
+        }
     });
 });

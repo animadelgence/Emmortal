7166e55d229a87cd6d130d68bf83c713813a0e1b
shilpitachatterjee <shilpitadelgence@gmail.com>
2017-06-21 15:44:03 +0530

    tributecreate

--- public/js/Album/photomodal.js
+++ public/js/Album/photomodal.js
@@ -10,8 +10,7 @@
 /*jshint -W065 */
 var base_url_dynamic = window.location.origin;
 $(document).ready(function () {
-    //alert();
-    $('#div-editphoto').hide();
+    $('#div-editphoto').hide(); 
     CKEDITOR.replace('imagetextDescription', {
         toolbar: [
 

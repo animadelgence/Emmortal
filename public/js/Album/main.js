d1b07677db16944045e8b6b52428565bbc2d1941
animadelgence <anima.delgence@gmail.com>
2017-06-30 11:46:34 +0530

    image upload modal click

--- public/js/Album/main.js
+++ public/js/Album/main.js
@@ -1,18 +1,29 @@
+/*
+ * @Author: Anima
+ * @Date:   2017-06-29 10:46:35
+ * @Last Modified by:   shilpita
+ * @Last Modified time: 2017-06-29 16:46:35
+ */
+/*jslint browser: true*/
+/*global $, jQuery, alert,  Aviary, currentImage, csdkImageEditor, console, launchImageEditor,validateFileType, loadtheuploadbutton, showTheEditButtons, elementclick*/
+/*jslint plusplus: true */
+/*jshint -W065 */
+/*jshint -W030 */
+
+/*jslint eqeq: true*/
 function getRandomSize(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
+    return Math.round(Math.random() * (max - min) + min);
 }
 var gridster = "";
 
-    $(function () {
-
-
-gridster = $(".gridster ul").gridster({
-            namespace: '.gridster',
-            widget_base_dimensions: [182,181],
-            widget_margins: [10, 10],
-            max_cols:6
-        }).data('gridster').disable();
-    });
+$(function () {
+    gridster = $(".gridster ul").gridster({
+        namespace: '.gridster',
+        widget_base_dimensions: [182, 181],
+        widget_margins: [10, 10],
+        max_cols: 6
+    }).data('gridster').disable();
+});
 
 
 /*

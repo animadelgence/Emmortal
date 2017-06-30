b68d47820fdf34c766aed19f3a67c7cd792b63bb
animadelgence <anima.delgence@gmail.com>
2017-06-30 15:14:03 +0530

    background

--- public/js/Album/lazyload.js
+++ public/js/Album/lazyload.js
@@ -14,6 +14,7 @@
 
 
 function yHandler() {
+    console.log("dfdfg")
     var noofScroll = $('#valuetobeIncremented').val();
     var wrap = '';
     var contentHeight = '';
@@ -33,10 +34,10 @@ function yHandler() {
     /* jshint ignore:end */
     wrap = document.getElementById("outer-wrap");
     contentHeight = wrap.offsetHeight;
-    yOffset = window.pageYOffset;
-    y = yOffset + window.innerHeight;
+    yOffset = $("#fullwrapnew").pageYOffset;
+    y = yOffset + $("#fullwrapnew").innerHeight;
     no_of_template_div = $('#outer-wrap').children().length;
-
+console.log(yOffset+"--"+y+"--"+contentHeight);
     i = parseInt(no_of_template_div, 10);
 
     //id_string = $('#category_string').html();

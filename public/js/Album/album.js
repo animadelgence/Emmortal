832a08293007cdb52ff6184c7e34af101e53c1d1
Shubhadip Bera <shubhadip.d2s@gmail.com>
2017-07-03 16:50:04 +0530

    slider

--- public/js/Album/album.js
+++ public/js/Album/album.js
@@ -32,30 +32,55 @@ $(function () {
             html = "",
             row = 1;
         row = rowCount();
-        if (datacmd == 'text') {
-            textTitle = $(this).children().children().children('p:first-child').text();
-            textDescription = $(this).children().children().children().next().text();
-            html += '<p>' + textTitle + '</p><p>' + textDescription + '</p>';
-        } else if (datacmd == 'image') {
-            imageUrl = $(this).children().children().attr('src');
-            html += '<img class="img-responsive" src="' + imageUrl + '" alt="...">';
-        } else if (datacmd == 'video') {
-            videoUrl = $(this).children().children().attr('src');
-            html += '<video controls="controls" name="Video Name" id="" src="' + videoUrl + '" style="width:100%;height:100%;"></video>';
-
-        }
-        if ($('#slidermodal').length > 0) {
-            $('#slidermodal').modal('show');
-            $('#appendDiv').html(html);
-            arrowHideShow(count, datasizey, datasizex, datacol, datarow);
-        } else {
-            $.get(getUrl + "/modal/slidermodal.php", function (result) {
-                $('body').append(result);
-                $('#slidermodal').modal('show');
-                $('#appendDiv').html(html);
-                arrowHideShow(count, datasizey, datasizex, datacol, datarow);
-            });
-        }
+        $.ajax({
+            type: "POST",
+            url: getUrl + '/albumdetails/getupload',
+            data: {
+                datacmd: datacmd,
+                uploadId: uploadId
+            },
+            success: function (res) {
+                jsObject = JSON.parse(res);
+                var profileimage = "/image/profile-deafult-avatar.jpg";
+                if(jsObject.uploadDetails[0].userimage !=''){
+                    profileimage = jsObject.uploadDetails[0].userimage;
+                }
+                html +='<div class="col-md-12" style="margin:5px;background-color: #fff;margin-left: 21px;"><div class="col-md-1 item active"><img src="'+profileimage+'" style="border-radius: 25px;height:50px;width:50px;margin-top:22px;"alt="" title=""></div><div class="col-md-11 item active"><h2 style="font-size: 20px;">'+jsObject.uploadDetails[0].uploadTitle+'</h2><p>by '+jsObject.uploadDetails[0].username+' on '+jsObject.uploadDetails[0].dateTime+'</p></div></div><div class="col-md-12" style="min-height: 500px;margin-top:10px;padding:0px;"><div class="col-md-6" style="min-height: 200px;"><div class="col-md-12" style="min-height: 200px;margin: 5px;background-color: #fff;border: 1px solid #ddd;"><div class="item active" style="line-height: 1.42857143;background-color: #fff;border: 1px solid #ddd;border-radius: 4px;min-height: 200px;margin: 17px 7px 17px 7px;">';
+                if (datacmd == 'text') {
+                    var textTitle = jsObject.uploadDetails[0].uploadTitle;
+                    var textDescription = jsObject.uploadDetails[0].uploadDescription;
+                    html += '<p style="margin-left: 10px;margin-top: 5px;font-size: 20px;">' + textTitle + '</p><p style="margin-left: 10px;margin-top: 5px;">' + textDescription + '</p>';
+                } else if (datacmd == 'image') {
+                    var imageUrl = jsObject.uploadDetails[0].uploadPath;
+                    html += '<img class="img-responsive" src="' + imageUrl + '" alt="...">';
+                } else if (datacmd == 'video') {
+                    var videoUrl = jsObject.uploadDetails[0].uploadPath;
+                    html += '<video controls="controls" name="Video Name" id="" src="' + videoUrl + '" style="width:100%;height:100%;"></video>';
+                }
+                
+              html +='</div></div><div class="col-md-12" style="margin-top: 20px;"><p>'+jsObject.uploadDetails[0].uploadDescription+'</p></div>';
+                html +='<div class="col-md-12" style="margin-top:5px;border-bottom:1px solid #ddd;"><p><span>11</span> <strong>Responses</strong></p></div>';
+                        html +='<div class="col-md-12" style="margin-top:5px;border-bottom:1px solid #ddd;"><div class="col-md-2 item active" style="padding: 0;"><img src="/image/profile-deafult-avatar.jpg" style="border-radius: 25px;height:50px;width:50px;margin-top:22px;"alt="" title=""></div><div class="col-md-10 item active"><h2 style="font-size: 20px;">commented By</h2><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p></div></div>';
+                html +='</div>';
+                html +='<div class="col-md-6" style="min-height:200px;"><div class="col-md-12" ><ul><li style="margin-top:5px;border-bottom:1px solid #ddd;"><span><img src="/image/like.png"><span> '+jsObject.uploadDetails[0].likeCount+' likes</span></span></li></ul></div></div></div>';
+                if ($('#slidermodal').length > 0) {
+                    $('#slidermodal').modal('show');
+                    $('#appendDiv').html(html);
+                    $("#appendDiv").show("slide", { direction: "left" }, 1000).parent().siblings().hide("slide", { direction: "left" }, 1000);
+                    arrowHideShow(count, datasizey, datasizex, datacol, datarow);
+                } else {
+                    $.get(getUrl + "/modal/slidermodal.php", function (result) {
+                        $('body').append(result);
+                        $('#slidermodal').modal('show');
+                        $('#appendDiv').html(html);
+                        $("#appendDiv").show("slide", { direction: "left" }, 1000).parent().siblings().hide("slide", { direction: "left" }, 1000);
+                        arrowHideShow(count, datasizey, datasizex, datacol, datarow);
+                    });
+                }
+            }
+        });
+        
+        
     });
 
 

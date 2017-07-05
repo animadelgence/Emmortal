ef354cce0191b9a2c455714d71ef6035e9ec17dd
shilpitachatterjee <shilpitadelgence@gmail.com>
2017-07-05 16:48:03 +0530

    gallery view

--- public/js/profile/modalload.js
+++ public/js/profile/modalload.js
@@ -1,3 +1,16 @@
+/*
+ * @Author: shilpita
+ * @Date:   2017-06-19 18:46:35
+ * @Last Modified by:   shilpita
+ * @Last Modified time: 2017-06-19 18:52:26
+ */
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+/*jslint plusplus: true */
+/*jslint indent: 4, maxerr: 50, vars: true, regexp: true, sloppy: true */
+/*jshint -W065 */
+/*jslint devel: true */
+/*jslint eqeq: true*/
 var getUrl = window.location.origin;
 $(function(){
 
@@ -22,6 +35,58 @@ function squarespaceModal2open()
         // open modal
         $('#squarespaceModal').modal('hide');
         $('#squarespaceModal2').modal('show');
+
+    });
+}
+
+function relationshipsmodal()
+{
+    if($('#relationshipsmodal').length) {
+        $('#relationshipsmodal').remove();
+    }
+    $.get(getUrl+"/modal/relationshipsmodal.php", function (result) {
+        // append response to body
+        $('body').append(result);
+        // open modal
+        $('#relationshipsmodal').modal('show');
+        var name = $('.relationships').attr('data-name');
+        $(".firstName").html(name);
+        friendlist('AllFriend', '');
+
+    });
+}
+
+function albumdetailsmodal()
+{
+    if($('#albumdetailsmodal').length) {
+        $('#albumdetailsmodal').remove();
+    }
+    $.get(getUrl+"/modal/albumdetailsmodal.php", function (result) {
+        // append response to body
+        $('body').append(result);
+        // open modal
+        $('#albumdetailsmodal').modal('show');
+        var name = $('.relationships').attr('data-name');
+        var id = $('.relationships').attr('id');
+        $.ajax({
+        type: "POST",
+        url: getUrl + '/album/fetchAllAlbum',
+        success: function (res) {
+            var jsObject = JSON.parse(res);
+                 var   i = 0;
+                 var   appendHtml = "";
+                 var i = jsObject.uploadDetails.length;
+                    appendHtml += '<div class="m-t-5 ng-scope"><div class="album-preview ng-isolate-scope"><div class="album-preview-cover-wrapper m-r-10"><img class="img-responsive" src="'+getUrl+'/image/no_cover-e343970a522a1599bd04bb0453d26b90.jpg"></div><div class="album-preview-info"><a class="album-preview-title font-bold e-link ng-binding" href="">My chronicles</a><div class="e-brown m-b-10"><small class="album-preview-location"></small></div><div class="action-btns"><div tooltip-placement="bottom" tooltip="Likes" class="e-like btn e-btn btn-round full ng-binding ng-isolate-scope">0</div><div tooltip="Tributes" tooltip-placement="bottom" class="btn e-btn btn-brown btn-round full ng-binding ng-isolate-scope" content-id="47" >0</div></div></div><div class="album-preview-collection">';
+           while(i == 3){
+             appendHtml += ' <div class="image-wrapper ng-scope"><img class="img-responsive" src="'+jsObject.uploadDetails[0].uploadPath+'"></div></div>';
+
+           }
+          appendHtml += '</div></div></div></div>';
+          
+           $('.append_div').append(appendHtml);
+        }
+    });
+        $(".firstName").html(name);
 
     });
 }

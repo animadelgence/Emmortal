e821d70be9ec23f8e7e8baa394ac878b25a4e88b
shilpitachatterjee <shilpitadelgence@gmail.com>
2017-07-07 15:07:28 +0530

    gallery

--- public/js/profile/modalload.js
+++ public/js/profile/modalload.js
@@ -84,7 +84,7 @@ function albumdetailsmodal()
                  var   appendHtml = "";
                  var k = jsObject.uploadDetails.length;
                  if(k){
-                    appendHtml += '<div class="m-t-5 ng-scope"><div class="album-preview ng-isolate-scope"><div class="album-preview-cover-wrapper m-r-10"><img class="img-responsive" src="'+getUrl+'/image/no_cover-e343970a522a1599bd04bb0453d26b90.jpg"></div><div class="album-preview-info"><a class="album-preview-title font-bold e-link ng-binding" href="href="'+getUrl+'/createalbum/showafterpublish/'+encodeUploadIdStatic+'"">My chronicles</a><div class="e-brown m-b-10"><small class="album-preview-location"></small></div><div class="action-btns"><div tooltip-placement="bottom" tooltip="Likes" class="e-like btn e-btn btn-round full ng-binding ng-isolate-scope">0</div><div tooltip="Tributes" tooltip-placement="bottom" class="btn e-btn btn-brown btn-round full ng-binding ng-isolate-scope" content-id="47" >0</div></div></div>';
+                    appendHtml += '<div class="m-t-5 ng-scope"><div class="album-preview ng-isolate-scope"><div class="album-preview-cover-wrapper m-r-10"><img class="img-responsive" src="'+getUrl+'/image/no_cover-e343970a522a1599bd04bb0453d26b90.jpg"></div><div class="album-preview-info"><a class="album-preview-title font-bold e-link ng-binding" href="'+getUrl+'/createalbum/showafterpublish/'+encodeUploadIdStatic+'"">My chronicle</a><div class="e-brown m-b-10"><small class="album-preview-location"></small></div><div class="action-btns"><div tooltip-placement="bottom" tooltip="Likes" class="e-like btn e-btn btn-round full ng-binding ng-isolate-scope">0</div><div tooltip="Tributes" tooltip-placement="bottom" class="btn e-btn btn-brown btn-round full ng-binding ng-isolate-scope" content-id="47" >0</div></div></div>';
             appendHtml += '<div class="album-preview-collection">';
                 
                 for(var l = 0; l < k; l++){
@@ -92,9 +92,9 @@ function albumdetailsmodal()
                 if(l <3){
               
                     if(jsObject.uploadDetails[l].uploadDetails[0].uploadType == "image"){
-                        appendHtml += ' <div class="image-wrapper ng-scope"><img class="img-responsive" src="'+jsObject.uploadDetails[l].uploadDetails[0].uploadPath+'" style="height:100%;"></div>';
+                        appendHtml += ' <div class="image-wrapper ng-scope"><img class="img-responsive" src="'+jsObject.uploadDetails[l].uploadDetails[0].uploadPath+'" style="height:100px%;"></div>';
                     } else if(jsObject.uploadDetails[l].uploadDetails[0].uploadType == "video"){
-                        appendHtml += ' <div class="image-wrapper ng-scope"><video controls="controls" name="Video Name" id="" src="'+jsObject.uploadDetails[l].uploadDetails[0].uploadPath+'" style="height:100%;"></video></div>';
+                        appendHtml += ' <div class="image-wrapper ng-scope"><video controls="controls" name="Video Name" id="" src="'+jsObject.uploadDetails[l].uploadDetails[0].uploadPath+'" style="height: 102px;max-width: 100px;"></video></div>';
                     }
                           else if(jsObject.uploadDetails[l].uploadDetails[0].uploadType == "text"){
                         appendHtml += ' <div class="image-wrapper ng-scope"><label name="text Name" style="height:100%;"><p>'+jsObject.uploadDetails[l].uploadDetails[0].uploadTitle+'</p><p>'+jsObject.uploadDetails[l].uploadDetails[0].uploadDescription+'</p></label></div>';
@@ -120,9 +120,9 @@ function albumdetailsmodal()
                   if(jsObject.albumValue[n].uploadDetails[t].uploadType != "album"){ 
                 if(t <= 3){
                     if(jsObject.albumValue[n].uploadDetails[t].uploadType == "image"){
-                        appendHtml += ' <div class="image-wrapper ng-scope"><img class="img-responsive" src="'+jsObject.albumValue[n].uploadDetails[t].uploadPath+'" style="height:100%;"></div>';
+                        appendHtml += ' <div class="image-wrapper ng-scope"><img class="img-responsive" src="'+jsObject.albumValue[n].uploadDetails[t].uploadPath+'" style="height:100px;"></div>';
                     } else if(jsObject.albumValue[n].uploadDetails[t].uploadType == "video"){
-                        appendHtml += ' <div class="image-wrapper ng-scope"><video controls="controls" name="Video Name" id="" src="'+jsObject.albumValue[n].uploadDetails[t].uploadPath+'" style="height:50px;""></video></div>';
+                        appendHtml += ' <div class="image-wrapper ng-scope"><video controls="controls" name="Video Name" id="" src="'+jsObject.albumValue[n].uploadDetails[t].uploadPath+'" style="height: 102px;max-width: 100px;"></video></div>';
                     }
                           else if(jsObject.albumValue[n].uploadDetails[t].uploadType == "text"){
                         appendHtml += ' <div class="image-wrapper ng-scope"><label name="text Name" style="height:100%;"><p>'+jsObject.albumValue[n].uploadDetails[t].uploadTitle+'</p><p>'+jsObject.albumValue[n].uploadDetails[t].uploadDescription+'</p></label></div>';
